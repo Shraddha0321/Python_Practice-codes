4ae9344a5ec592bbe95c11a7204fa25c5dfb77a1
Shraddha_kedare <69896482+Shraddha0321@users.noreply.github.com>
2024-06-05 10:44:07 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+a=int(input("Enter first Number:"))
+b=int(input("Enter second Number:"))
+
+
+ans1= a+b
+print("Addition:",ans1)
+
+ans2=a-b
+print("Subtraction:",ans2)
+
+ans3=a*b
+print("Multiplication:",ans3)
+
+ans4=a/b
+print("division:",ans4)
+
+ans5=a%b
+print("Modules:",ans4)
+
+ans=a//b
+print("Floor:",ans5)
+
